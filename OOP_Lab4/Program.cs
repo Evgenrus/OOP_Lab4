e503142f74ac787c50b605b8c9d8df10a630a568
Evgen <evgenrus257@yandex.ru>
2021-05-01 16:55:58 +0700

    fraction class done

--- OOP_Lab4/Program.cs
+++ OOP_Lab4/Program.cs
@@ -7,6 +7,13 @@ namespace OOP_Lab4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Fraction a = new Fraction(1, 1, 0);
+            Fraction b = new Fraction(4, 2, 0);
+            b.shorten();
+            b.show();
+            a.show();
+            (a+b).show();
+            (a/b).show();
         }
     }
 }
--- OOP_Lab4/fraction.cs
+++ OOP_Lab4/fraction.cs
@@ -1,4 +1,7 @@
-﻿namespace OOP_Lab4
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace OOP_Lab4
 {
     public class Fraction
     {
@@ -6,36 +9,39 @@
         private int denom;
         private int integ;
 
-        public Fraction(int a = 0, int b = 1, int integ = 0)
+        public Fraction(int a = 0, int b = 1, int inte = 0)
         {
             nom   = a;
             denom = b;
-            this.integ     = integ;
-            this.shorten();
+            integ = inte;
         }
 
         public Fraction(Fraction previous) 
             : this(previous.integ, previous.denom, previous.integ)
         {}
 
-        public void normalize()
+        public Fraction normalize()
         {
-            integ = nom / denom;
-            nom /= denom;
-            shorten();
+            if (nom > denom)
+            {
+                integ = nom / denom;
+                nom /= denom;
+            }
+
+            return this;
         }
 
-        public void unNormalize()
+        public Fraction unNormalize()
         {
-            if (integ == 0)
-                return;
-            nom += integ * denom;
+            if(integ != 0)
+                nom += integ * denom;
+            return this;
         }
 
-        public void shorten()
+        public Fraction shorten()
         {
             int temp = 1;
-            for (int i = 0; i < nom; i++)
+            for (int i = 1; i <= Math.Min(nom, denom); i++)
             {
                 if (nom % i == 0 && denom % i == 0)
                     temp = i;
@@ -43,6 +49,33 @@
 
             nom   /= temp;
             denom /= temp;
+
+            return this;
+        }
+
+        public int toSameDenom(ref Fraction B)
+        {
+            int denominator = this.denom * B.denom;
+            this.nom *= B.denom;
+            B.nom *= this.denom;
+            this.denom = denominator;
+            B.denom = denominator;
+
+            return denominator;
+        }
+
+        public static Fraction operator +(Fraction A, Fraction B)
+        {
+            int denominator = A.toSameDenom(ref B);
+
+            return new Fraction(A.nom + B.nom, denominator, A.integ + B.integ);
+        }
+
+        public static Fraction operator -(Fraction A, Fraction B)
+        {
+            int denominator = A.toSameDenom(ref B);
+
+            return new Fraction(A.nom - B.nom, denominator, A.integ - B.integ);
         }
 
         public static Fraction operator *(Fraction A, Fraction B)
@@ -53,7 +86,14 @@
         public static Fraction operator /(Fraction A, Fraction B)
         {
             A.unNormalize(); B.unNormalize();
-            
+            return new Fraction(A.nom * B.denom, A.denom * B.nom).normalize();
+        }
+
+        public void show()
+        {
+            Console.WriteLine($"\ninteger: {integ};");
+            Console.WriteLine($"nominator: {nom};");
+            Console.WriteLine($"denominator: {denom};\n");
         }
     }
 }
